d0af59543dd272a730dd027d4e42e410b58e2ef4
diak <diak@mesterlevel.lo>
2026-03-05 09:05:12 +0100

    userController es a userRoutes bovult a whoami-al

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const userRoutes=require('./routes/userRoutes.js')
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ['http://127.0.0.1:5173&#39', 'http://localhost:5173&#39' ],
+    origin: ['http://127.0.0.1:5173', 'http://localhost:5173' ],
     credentials: true
 }))
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const {register, login, logout} = require('../controllers/userController')
+const {register, login, logout, whoAmI} = require('../controllers/userController')
 const { auth } = require('../middleware/userMiddleware')
 
 const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
-router.post('logout', auth, logout)
+router.get('/whoami', auth, whoAmI)
+router.post('/logout', auth, logout)
 
 module.exports = router
