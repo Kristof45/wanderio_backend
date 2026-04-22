a1ef07d74018ee050a50c54b598e7b7077786d53
diak <diak@mesterlevel.lo>
2026-04-22 13:29:54 +0200

    app.js cors-ba beleraktam a netlify linket, attraction modelben eliras javitas

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const hotelOrdersRoutes = require('./routes/hotelOrdersRoutes')
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ['http://127.0.0.1:5173', 'http://localhost:5173' ],
+    origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'https://extraordinary-choux-8610be.netlify.app/' ],
     credentials: true
 }))
 app.use(express.static('public'));
