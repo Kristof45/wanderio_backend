4a50be5a441682727ddbe6205f5f802fc6cfa413
vendel1123 <vendelhegyi68@gmail.com>
2026-04-27 18:56:31 +0200

    order

--- models/ticketOrderModel.js
+++ models/ticketOrderModel.js
@@ -1,7 +1,7 @@
 const db = require('../db/db')
 
 async function getTicketOrders() {
-    const sql = 'SELECT t.orderID, t.userID, u.username, f.flightsId, f.starting, f.arivval, a.airline AS airlineName, dep_city.name AS departureCityName, dest_city.name AS destinationCityName, t.status FROM ticketOrders AS t JOIN users AS u ON t.userID = u.userID JOIN flights AS f ON t.flightsId = f.flightsId JOIN airlines AS a ON f.airlineID = a.airlineID JOIN cities AS dep_city ON f.departureCityID = dep_city.cityID JOIN cities AS dest_city ON f.destinationCityID = dest_city.cityID ORDER BY t.orderID DESC;'
+    const sql = 'SELECT t.orderID, t.userID, u.username, f.flightsId, f.starting, f.arivval, a.airline AS airlineName, dep_city.name AS departureCityName, dest_city.name AS destinationCityName, t.status FROM ticketorders AS t JOIN users AS u ON t.userID = u.userID JOIN flights AS f ON t.flightsId = f.flightsId JOIN airlines AS a ON f.airlineID = a.airlineID JOIN cities AS dep_city ON f.departureCityID = dep_city.cityID JOIN cities AS dest_city ON f.destinationCityID = dest_city.cityID ORDER BY t.orderID DESC;'
     const [result] = await db.query(sql)
 
     return result
